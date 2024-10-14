9be4c3235935300782ce7b9bf1ff2ed14175b093
asofuoglu <a.sofuoglu1@gmail.com>
2024-10-14 16:34:47 +0300

    added new scr

--- src/test/java/starter/pageObjects/ing/Pin.java
+++ src/test/java/starter/pageObjects/ing/Pin.java
@@ -1,23 +1,18 @@
 package starter.pageObjects.ing;
 
 import static net.serenitybdd.core.Serenity.getDriver;
+import static starter.pageObjects.ing.PinsPage.*;
 
 import java.time.Duration;
-import net.serenitybdd.core.annotations.findby.By;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Performable;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.Tasks;
 import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.matchers.WebElementStateMatchers;
-import net.serenitybdd.screenplay.targets.Target;
 import net.serenitybdd.screenplay.waits.WaitUntil;
-import net.thucydides.core.webdriver.shadow.ByShadow;
 import org.openqa.selenium.JavascriptException;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import starter.Utils;
 
 public class Pin implements Task {
@@ -32,25 +27,40 @@ public class Pin implements Task {
     for (int i = 0; i < 3; i++) {
       // Click.on(PinsPage.NUMBER_ONE);
       // Evaluate.javascript(PinsPage.ONE );
-      selectPinOne();
+      try {
+        actor.attemptsTo(
+            WaitUntil.the(NUMBER_ONE, WebElementStateMatchers.isVisible())
+                .forNoMoreThan(Duration.ofSeconds(10)),
+            Click.on(NUMBER_ONE));
+      } catch (Exception ex) {
+        actor.attemptsTo(
+            WaitUntil.the(NUMBER_ONE_ALTERNATIVE, WebElementStateMatchers.isVisible())
+                .forNoMoreThan(Duration.ofSeconds(10)),
+            Click.on(NUMBER_ONE));
+      }
     }
+    // selectPinOne();
     // Click.on(PinsPage.CONTINUE_BUTTON);
-    clickOnContinue();
+    // clickOnContinue();
+    try {
+      actor.attemptsTo(
+          WaitUntil.the(CONTINUE_BUTTON, WebElementStateMatchers.isVisible())
+              .forNoMoreThan(Duration.ofSeconds(10)),
+          Click.on(CONTINUE_BUTTON));
+    } catch (Exception ex) {
+      actor.attemptsTo(
+          WaitUntil.the(CONTINUE_BUTTON_ALTERNATIVE, WebElementStateMatchers.isVisible())
+              .forNoMoreThan(Duration.ofSeconds(10)),
+          Click.on(CONTINUE_BUTTON_ALTERNATIVE));
+    }
   }
 
   public static Performable select() {
     return Tasks.instrumented(Pin.class);
   }
 
-  private void selectPinOne() {
+  private void selectPinOneByJs() {
     try {
-      WaitUntil.the(
-              Target.the("Pin number one -1")
-                      .located(ByShadow.cssSelector("button[aria-label=\"Uno\"]", "#meansContainer > ing-orange-it-pinpad")),
-              WebElementStateMatchers.isVisible()
-      ).forNoMoreThan(Duration.ofSeconds(10));
-      Click.on(Target.the("Pin number one -1")
-              .located(ByShadow.cssSelector("button[aria-label=\"Uno\"]", "#meansContainer > ing-orange-it-pinpad")));
       js.executeScript(
           "document.querySelector('#meansContainer > ing-orange-it-pinpad')."
               + "shadowRoot.querySelector('.pinpad-keyboard')."
@@ -69,15 +79,7 @@ public class Pin implements Task {
     }
   }
 
-  private void waitUntilPinPadVisible() {
-    WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(30));
-    WebElement shadowHost =
-        wait.until(
-            ExpectedConditions.visibilityOfElementLocated(
-                By.cssSelector("#meansContainer > ing-orange-it-pinpad")));
-  }
-
-  private void clickOnContinue() {
+  private void clickOnContinueByJs() {
     try {
       js.executeScript(
           "document.querySelector('#meansContainer > ing-orange-it-pinpad').shadowRoot.querySelector('.continue__button').click();");
--- src/test/java/starter/pageObjects/ing/PinsPage.java
+++ src/test/java/starter/pageObjects/ing/PinsPage.java
@@ -11,11 +11,27 @@ public class PinsPage {
                   "button[aria-label=\"Uno\"]",
                   "#meansContainer > ing-orange-it-pinpad",
                   ".pinpad-keyboard"));
+
+  public static final Target NUMBER_ONE_ALTERNATIVE =
+      Target.the("Pin number one -1 alternative")
+          .located(
+              ByShadow.cssSelector(
+                  "button[aria-label=\"Uno\"]",
+                  "#meansContainer > ing-orange-it-secure-otp",
+                  "smsotp-pinpad",
+                  ".pinpad-keyboard"));
   public static final Target CONTINUE_BUTTON =
       Target.the("Continue button")
           .located(
               ByShadow.cssSelector(".continue__button", "#meansContainer > ing-orange-it-pinpad"));
 
+  public static final Target CONTINUE_BUTTON_ALTERNATIVE =
+      Target.the("Continue button alternative")
+          .located(
+              ByShadow.cssSelector(
+                  ".continue__button",
+                  "#meansContainer > ing-orange-it-secure-otp",
+                  "smsotp-footer"));
   public static final Target PINPAD_HEADER =
       Target.the("Pin pad Header text")
           .located(ByShadow.cssSelector(".position-header-text", "ing-orange-it-pinpad"));
